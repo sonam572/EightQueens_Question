df316dda1a7219e8dcddb593282a3ec1bc156de8
ACER <ACER@ACER-PC>
2020-08-07 15:43:51 +0530

    Initial-Commit

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -1,8 +1,68 @@
+import java.awt.Color;
+
 public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
-
-		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
-	}
-}
+		if(square<8) {
+		  if(square%2==0) {
+  		System.out.println(square);
+		return true;}
+		  else {
+			  return false;
+		  }}
+		else if(square>=8&&square<16){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return false;}
+				  else {
+					  return true;
+				  }
+		}
+		else if(square>=16&&square<24){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return true;}
+				  else {
+					  return false;
+				  }
+		}
+		else if(square>=24&&square<32){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return false;}
+				  else {
+					  return true;
+				  }
+		}
+		else if(square>=32&&square<40){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return true;}
+				  else {
+					  return false;
+				  }
+		}
+		else if(square>=40&&square<48){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return false;}
+				  else {
+					  return true;
+				  }
+		}
+		else if(square>=48&&square<56){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return true;}
+				  else {
+					  return false;
+				  }
+		}
+		else if(square>=56&&square<64){
+			if(square%2==0) {
+		  		System.out.println(square);
+				return false;}
+				  else {
+					  return true;
+				  }
+		}
+		return false;}}
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -1,19 +1,48 @@
 public class GameStrategy {
 	private boolean placedQueens[][] = new boolean[8][8];
 	private int numQueens = 0;
-
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
-	}
-	
-	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
+		if(cellId<8) {
+			return 0;}
+		if(cellId>=8&&cellId<16) {
+			return 1;}
+		if(cellId>=16&&cellId<24) {
+			return 2;}
+		if(cellId>=24&&cellId<32) {
+			return 3;}
+		if(cellId>=32&&cellId<40) {
+			return 4;}
+		if(cellId>=40&&cellId<48) {
+			return 5;}
+		if(cellId>=48&&cellId<56) {
+			return 6;}
+		if(cellId>=56&&cellId<64) {
+			return 7;}
 		
-		return 0;	// just for the heck of it
+	return 0;// just for the heck of it
+	
 	}
-
+	private int getRow(int cellId) {
+		if(cellId<8) {
+			return 0;
+		}
+		if(cellId>=8&&cellId<16) {
+			return 1;}
+		if(cellId>=16&&cellId<24) {
+			return 2;}
+		if(cellId>=24&&cellId<32) {
+			return 3;}
+		if(cellId>=32&&cellId<40) {
+			return 4;}
+		if(cellId>=40&&cellId<48) {
+			return 5;}
+		if(cellId>=48&&cellId<56) {
+			return 6;}
+		if(cellId>=56&&cellId<64) {
+			return 7;}
+		return 0;// just for the heck of it
+		
+		}
 	public boolean isValidPosition(int cellId) {
 		boolean isValid = true;
 
@@ -25,12 +54,44 @@ public class GameStrategy {
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
-		return isValid;
+		int i, j; 
+		int q[]=new int[8];
+		 
+        
+		for (i = 0; i < row; i++) {
+            if (q[i] == q[row])             return false;   // same column
+            if ((q[i] - q[row]) == (row- i)) return false;   // same major diagonal
+            if ((q[row] - q[i]) == (row - i)) return false;   // same minor diagonal
+        }
+		
+        
+   		return isValid;
 	}
+	public void enumerate(int[] q, int k) {
+        int n = q.length;
+        if (k == n) printQueens(q);
+        else {
+            for (int i = 0; i < n; i++) {
+                q[k] = i;
+                if (isValidPosition(k)) enumerate(q, k+1);
+            }
+        }
+    }  
+	public void enumerate() {
+        int[] a = new int[8];
+        enumerate(a, 0);
+    }
+	public static int printQueens(int[] q) {
+        int n = q.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (q[i] == j) return 1;
+                else           return 0;}
+            }  
+        return 1;
+        }
+
+
 }
 
 
